d8525310abbdcdd7daff0484e65a32a64d98c53b
Felipe Baracaldo <felipe.baracaldo1118@icloud.com>
2026-09-21 11:59:01 -0500

    finishing modals with also keyboard events

--- project2/script.js
+++ project2/script.js
@@ -20,3 +20,15 @@ const close = () => {
 
 btn.addEventListener("click", close);
 overlay.addEventListener("click", close);
+
+//to use KEYBOARD EVENTS we also need to take into addEventListener.
+//this method hear whenever the event it's happening around the page.
+
+document.addEventListener("keydown", function (e) {
+  //we can pass the event as an argument
+  //basically here, we are storaging whenever the even is being executed and also to be used whenever we need it
+
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    close();
+  }
+});
