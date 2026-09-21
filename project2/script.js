8fb3e115d1e15862b22a2014f3734a58aec7d055
Felipe Baracaldo <felipe.baracaldo1118@icloud.com>
2026-09-21 11:10:43 -0500

    working with classes

--- project2/script.js
+++ project2/script.js
@@ -5,4 +5,18 @@ const overlay = document.querySelector(".overlay");
 const btn = document.querySelector(".close-modal");
 const show = document.querySelectorAll(".show-modal");
 
-console.log(show);
+const openModal = () => {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+for (let i = 0; i < show.length; i++) {
+  show[i].addEventListener("click", openModal);
+}
+const close = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btn.addEventListener("click", close);
+overlay.addEventListener("click", close);
